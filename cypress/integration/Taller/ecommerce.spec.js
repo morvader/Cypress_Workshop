667255ad9c1a5116f87955b21cb05d6423dec07b
Francisco Moreno <francisco.moreno@sngular.com>
2021-06-01 09:22:54 +0200

    cambio web

--- cypress/integration/Taller/ecommerce.spec.js
+++ cypress/integration/Taller/ecommerce.spec.js
@@ -2,16 +2,15 @@
 
 context("Busqueda de productos", () => {
   beforeEach("go to home page", () => {
-    cy.visit("http://automationpractice.com/index.php");
+    cy.visit("http://opencart.abstracta.us/");
   });
 
-  it("La blusa se muestra cone l nombre correcto", () => {
-    cy.get("#search_query_top").type("Blouse").get("#searchbox > .btn").click();
+  it("Búsqueda de los componentes correctos", () => {
+    cy.get("[name='search']")
+      .type("phone")
+      .get(".input-group-btn > .btn")
+      .click();
 
-    // Este localizador es más robusto que el que recomienda Cypress
-    cy.get(".product_list > li:first-child .product-name").should(
-      "contain.text",
-      "Blouse"
-    );
+    cy.get(".product-thumb .caption > h4 > a").should("have.text", "iPhone");
   });
 });
